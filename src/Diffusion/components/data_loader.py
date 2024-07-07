4b467e0f309faf115bf795afda0e5133c0b2e4a0
AmzadHossainrafis <amzad.rafi@northsouth.edu>
2024-07-07 13:02:49 +0600

    chore: Add data loader function for dataset

---
+++ src/Diffusion/components/data_loader.py
@@ -0,0 +1,22 @@
+from torch.utils.data import DataLoader 
+import torchvision
+
+def get_data(dataset_dir, transforms):
+    """
+    Returns a DataLoader object for the dataset at the specified directory.
+
+    This function takes the path to a dataset directory and a set of transformations,
+    and returns a DataLoader object for the dataset. The DataLoader object is created
+    using the ImageFolder class from torchvision.datasets module.
+
+    Parameters:
+        dataset_dir (str): The path to the dataset directory.
+        transforms (torchvision.transforms.Compose): A composition of image transformations.
+
+    Returns:
+        DataLoader: A DataLoader object for the dataset.
+    """ 
+
+    dataset = torchvision.datasets.ImageFolder(dataset_dir , transform=transforms)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    return dataloader
