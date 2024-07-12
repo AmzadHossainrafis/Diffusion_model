2c959a95969700f208a9befcc902fd6a6e900db4
AmzadHossainrafis <amzad.rafi@northsouth.edu>
2024-07-12 12:49:15 +0600

    Refactor code to remove unused imports and commented out code

--- src/Diffusion/components/data_loader.py
+++ src/Diffusion/components/data_loader.py
@@ -1,10 +1,12 @@
-from torch.utils.data import DataLoader
 import torchvision
+from torch.utils.data import DataLoader
 from Diffusion.components.data_transformation import transforms
 from Diffusion.utils.logger import logger
+from Diffusion.utils.utils import read_config
 
+data_config = read_config("/home/amzad/Desktop/diffusion/config/config.yaml")["Train_config"]
 
-def get_data(dataset_dir, batch_size=2, shuffle=True, transforms=transforms):
+def get_data(dataset_dir, batch_size=data_config['batch_size'], shuffle=True, transforms=transforms):
     """
     Returns a DataLoader object for the dataset at the specified directory.
 
